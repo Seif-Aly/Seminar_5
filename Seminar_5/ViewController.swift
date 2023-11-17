daada7931f7ec1c078da98f522de565ed9071f92
seif <saly@edu.hse.ru>
2023-11-17 18:13:31 +0300

    Initial Commit

---
+++ Seminar_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar_5
+//
+//  Created by seif on 2023-11-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
